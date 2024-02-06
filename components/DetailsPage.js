fb0e20ac9521737b6b3277bc3ac886315dc64cf8
jack9221 <>
2024-02-06 15:07:57 +0800

    Finish app

---
+++ components/DetailsPage.js
@@ -0,0 +1,42 @@
+import { getProductById } from '/services/Menu.js';
+
+export class DetailsPage extends HTMLElement {
+    constructor() {
+        super();
+
+        this.root = this.attachShadow({ mode: 'open' });
+
+        const template = document.getElementById('details-page-template');
+        const content = template.content.cloneNode(true);
+        const styles = document.createElement('style');
+        this.root.appendChild(styles);
+        this.root.appendChild(content);
+
+        async function loadCSS() {
+            const request = await fetch('/components/DetailsPage.css');
+            styles.textContent = await request.text();
+        }
+        loadCSS();
+    }
+
+    connectedCallback() {
+        this.renderData();
+    }
+
+    async renderData() {
+        if (this.dataset.id) {
+            this.product = await getProductById(this.dataset.id);
+            this.root.querySelector('h2').textContent = this.product.name;
+            this.root.querySelector('img').src = `/data/images/${this.product.image}`;
+            this.root.querySelector('.description').textContent = this.product.description;
+            this.root.querySelector('.price').textContent = `$${this.product.price.toFixed(2)} ea`;
+            this.root.querySelector('button').addEventListener('click', () => {
+                console.log('Clicked!');
+            })
+        } else {
+            alert('Invalid Product ID');
+        }
+    }
+}
+
+customElements.define('details-page', DetailsPage);
